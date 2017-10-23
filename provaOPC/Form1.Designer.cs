deff03ba7bdfc8e125578bb59ceff6b79880e2d1
Tibe93 <francesco.tiberia@gmail.com>
2017-10-23 16:34:22 +0200

    c
    c

---
+++ provaOPC/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace provaOPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butScrivi = new System.Windows.Forms.Button();
+            this.butPath = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxArray = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxTopic = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // butScrivi
+            // 
+            this.butScrivi.Enabled = false;
+            this.butScrivi.Location = new System.Drawing.Point(267, 69);
+            this.butScrivi.Name = "butScrivi";
+            this.butScrivi.Size = new System.Drawing.Size(53, 23);
+            this.butScrivi.TabIndex = 0;
+            this.butScrivi.Text = "Scrivi";
+            this.butScrivi.UseVisualStyleBackColor = true;
+            this.butScrivi.Click += new System.EventHandler(this.butScrivi_Click);
+            // 
+            // butPath
+            // 
+            this.butPath.Location = new System.Drawing.Point(288, 12);
+            this.butPath.Name = "butPath";
+            this.butPath.Size = new System.Drawing.Size(32, 22);
+            this.butPath.TabIndex = 1;
+            this.butPath.Text = "...";
+            this.butPath.UseVisualStyleBackColor = true;
+            this.butPath.Click += new System.EventHandler(this.butPath_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.BackColor = System.Drawing.Color.LightGreen;
+            this.textBoxPath.Location = new System.Drawing.Point(13, 13);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(269, 22);
+            this.textBoxPath.TabIndex = 2;
+            this.textBoxPath.Text = "Selezionare il file .CSV da scrivere";
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBoxPath_TextChanged);
+            // 
+            // textBoxArray
+            // 
+            this.textBoxArray.Enabled = false;
+            this.textBoxArray.Location = new System.Drawing.Point(13, 41);
+            this.textBoxArray.Name = "textBoxArray";
+            this.textBoxArray.Size = new System.Drawing.Size(307, 22);
+            this.textBoxArray.TabIndex = 3;
+            this.textBoxArray.Text = "Inserire il nome dell\'Array su cui scrivere";
+            this.textBoxArray.Click += new System.EventHandler(this.textBoxArray_Click);
+            this.textBoxArray.TextChanged += new System.EventHandler(this.textBoxArray_TextChanged);
+            this.textBoxArray.Leave += new System.EventHandler(this.textBoxArray_Leave);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxTopic
+            // 
+            this.textBoxTopic.Enabled = false;
+            this.textBoxTopic.Location = new System.Drawing.Point(13, 70);
+            this.textBoxTopic.Name = "textBoxTopic";
+            this.textBoxTopic.Size = new System.Drawing.Size(248, 22);
+            this.textBoxTopic.TabIndex = 4;
+            this.textBoxTopic.Text = "Creg_OPC_Topic";
+            this.textBoxTopic.Click += new System.EventHandler(this.textBoxTopic_Click);
+            this.textBoxTopic.TextChanged += new System.EventHandler(this.textBoxTopic_TextChanged);
+            this.textBoxTopic.Leave += new System.EventHandler(this.textBoxTopic_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 102);
+            this.Controls.Add(this.textBoxTopic);
+            this.Controls.Add(this.textBoxArray);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.butPath);
+            this.Controls.Add(this.butScrivi);
+            this.MaximumSize = new System.Drawing.Size(350, 147);
+            this.MinimumSize = new System.Drawing.Size(350, 147);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butScrivi;
+        private System.Windows.Forms.Button butPath;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxArray;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxTopic;
+    }
+}
+
---
+++ provaOPC/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Opc.Da;
+using Opc;
+using System.IO;
+
+namespace provaOPC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static ItemValueResult RsLinx_OPC_Client_Read(string ItemName)
+        {
+            Opc.Da.Server server;
+            OpcCom.Factory fact = new OpcCom.Factory();
+            Opc.Da.Subscription groupRead;
+            Opc.Da.SubscriptionState groupState;
+            Opc.Da.Item[] items = new Opc.Da.Item[1];
+            // 1st: Create a server object and connect to the RSLinx OPC Server
+            server = new Opc.Da.Server(fact, null);
+            server.Url = new Opc.URL("opcda://localhost/RSLinx OPC Server/{A05BB6D6-2F8A-11D1-9BB0-080009D01446}");
+
+            //2nd: Connect to the created server
+            server.Connect();
+
+            //3rd Create a group if items            
+            groupState = new Opc.Da.SubscriptionState();
+            groupState.Name = "Group";
+            groupState.UpdateRate = 1000;// this isthe time between every reads from OPC server
+            groupState.Active = true;//this must be true if you the group has to read value
+            groupRead = (Opc.Da.Subscription)server.CreateSubscription(groupState);
+
+
+            // add items to the group    (in Rockwell names are identified like [Name of PLC in the server]ItemName)
+            items[0] = new Opc.Da.Item();
+            items[0].ItemName = ItemName;
+
+            items = groupRead.AddItems(items);
+            return groupRead.Read(items)[0];
+        }
+
+        public static void RsLinx_OPC_Client_Write(string ItemName, float Value)
+        {
+            Opc.Da.Server server;
+            OpcCom.Factory fact = new OpcCom.Factory();
+            Opc.Da.Subscription groupWrite;
+            Opc.Da.SubscriptionState groupStateWrite;
+            Opc.Da.Item[] items = new Opc.Da.Item[1];
+            // 1st: Create a server object and connect to the RSLinx OPC Server
+            server = new Opc.Da.Server(fact, null);
+            server.Url = new Opc.URL("opcda://localhost/RSLinx OPC Server/{A05BB6D6-2F8A-11D1-9BB0-080009D01446}");
+
+            //2nd: Connect to the created server
+            server.Connect();
+
+            // Create a write group            
+            groupStateWrite = new Opc.Da.SubscriptionState();
+            groupStateWrite.Name = "Group Write";
+            groupStateWrite.Active = false;//not needed to read if you want to write only
+            groupWrite = (Opc.Da.Subscription)server.CreateSubscription(groupStateWrite);
+
+            //Create the item to write (if the group doesn't have it, we need to insert it)
+            Opc.Da.Item[] itemToAdd = new Opc.Da.Item[1];
+            itemToAdd[0] = new Opc.Da.Item();
+            itemToAdd[0].ItemName = ItemName;
+
+            //create the item that contains the value to write
+            Opc.Da.ItemValue[] writeValues = new Opc.Da.ItemValue[1];
+            writeValues[0] = new Opc.Da.ItemValue(itemToAdd[0]);
+
+            //make a scan of group to see if it already contains the item
+            bool itemFound = false;
+            foreach (Opc.Da.Item item in groupWrite.Items)
+            {
+                if (item.ItemName == itemToAdd[0].ItemName)
+                {
+                    // if it find the item i set the new value
+                    writeValues[0].ServerHandle = item.ServerHandle;
+                    itemFound = true;
+                }
+            }
+            if (!itemFound)
+            {
+                //if it doesn't find it, we add it
+                groupWrite.AddItems(itemToAdd);
+                writeValues[0].ServerHandle = groupWrite.Items[groupWrite.Items.Length - 1].ServerHandle;
+            }
+            //set the value to write
+            writeValues[0].Value = Value;
+            //write
+            groupWrite.Write(writeValues);
+        }
+
+        private void butScrivi_Click(object sender, EventArgs e)
+        {
+            StreamReader File = new StreamReader(textBoxPath.Text);
+            string Ppm = openFileDialog1.SafeFileName.Split('_')[0];
+            File.ReadLine();//Salto la prima...
+            File.ReadLine();//... e la seconda riga perchè sono legende
+            string[] Line = new string[5];
+            for (int i = 0; i < 1250; i++)
+            {
+                Line = File.ReadLine().Split('\t');//Leggo la linea
+                RsLinx_OPC_Client_Write($"[{textBoxTopic.Text}]Posizione_{Ppm}[{i}]", float.Parse(Line[1]));//0 Time, 1 Pos, 2 Vel, 3 Cor
+                RsLinx_OPC_Client_Write($"[{textBoxTopic.Text}]Velocita_{Ppm}[{i}]", float.Parse(Line[2]));
+                RsLinx_OPC_Client_Write($"[{textBoxTopic.Text}]Corrente_{Ppm}[{i}]", float.Parse(Line[3]));
+            }
+        }
+
+        private void butPath_Click(object sender, EventArgs e)
+        {
+            //Utilizzo una finestra di dialogo per selezionare la cartella dove sono salvati i .CSV
+            openFileDialog1.ShowDialog();
+            textBoxPath.Text = openFileDialog1.FileName;
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+            //Operazioni sulla grafica
+            textBoxPath.BackColor = Color.White;
+            if (!textBoxPath.Text.Equals("Inserire Path Salvataggio .CSV"))
+            {
+                textBoxArray.Enabled = true;
+                textBoxArray.BackColor = Color.LightGreen;
+            }
+        }
+
+        private void textBoxArray_TextChanged(object sender, EventArgs e)
+        {
+            //Operazioni sulla grafica
+            if (!(textBoxArray.Text.Equals("Inserire il nome dell'Array su cui scrivere") || textBoxArray.Text.Equals("")))
+            {
+                textBoxTopic.Enabled = true;
+                if(!textBoxArray.Text.Equals("Inserire il nome del topic OPC"))
+                {
+                    butScrivi.Enabled = true;
+                }
+            }
+            textBoxArray.BackColor = Color.White;
+        }
+
+        private void textBoxArray_Leave(object sender, EventArgs e)
+        {
+            //Operazioni sulla grafica
+            if (textBoxArray.Text.Equals(""))
+            {
+                textBoxArray.Text = "Inserire il nome dell'Array su cui scrivere";
+                textBoxArray.BackColor = Color.LightGreen;
+                textBoxTopic.Enabled = false;
+            }
+        }
+
+        private void textBoxArray_Click(object sender, EventArgs e)
+        {
+            //Operazioni sulla grafica
+            if(textBoxArray.Text.Equals("Inserire il nome dell'Array su cui scrivere"))
+            {
+                textBoxArray.Text = "";
+            }
+        }
+
+        private void textBoxTopic_Click(object sender, EventArgs e)
+        {
+            //Operazioni sulla grafica
+            if (textBoxArray.Text.Equals("Inserire il nome del topic OPC"))
+            {
+                textBoxTopic.Text = "";
+            }
+        }
+
+        private void textBoxTopic_TextChanged(object sender, EventArgs e)
+        {
+            //Operazioni sulla grafica
+            if (!(textBoxTopic.Text.Equals("Inserire il nome del Topic OPC") || textBoxTopic.Text.Equals("")))
+            {
+                butScrivi.Enabled = true;
+            }
+            textBoxTopic.BackColor = Color.White;
+        }
+
+        private void textBoxTopic_Leave(object sender, EventArgs e)
+        {
+            //Operazioni sulla grafica
+            if (textBoxTopic.Text.Equals(""))
+            {
+                textBoxTopic.Text = "Inserire il nome del Topic OPC";
+                textBoxTopic.BackColor = Color.LightGreen;
+                butScrivi.Enabled = false;
+            }
+        }
+    }
+}
